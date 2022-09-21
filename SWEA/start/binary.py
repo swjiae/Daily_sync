35e5e84587ebe68223cec7781bfae899fe77408b
swjiae <jiae1625@gmail.com>
2022-09-22 07:39:36 +0900

    09/22

--- SWEA/start/binary.py
+++ SWEA/start/binary.py
@@ -5,7 +5,7 @@ for T in range(1, TC+1):
     bi = '' # 2진수 출력할 값
     while de > 0 and cnt < 13:
         temp = de * 2
-        bi += str(temp)[0]
+        bi += str(int(temp))
         de = temp - int(temp)
         cnt += 1
     if cnt == 13:
