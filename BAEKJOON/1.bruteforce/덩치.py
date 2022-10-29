e31748083bf11bccabd078468630d18a3cac9271
swjiae <jiae1625@gmail.com>
2022-10-29 23:11:02 +0900

    ~10/29

--- BAEKJOON/1.bruteforce/덩치.py
+++ BAEKJOON/1.bruteforce/덩치.py
@@ -4,3 +4,12 @@ for _ in range(N):
     x, y = map(int, input().split())
     arr.append([x, y])
 
+ranks = []
+for i in range(N):
+    rank = 1
+    for j in range(N):
+        if arr[i][0] < arr[j][0] and arr[i][1] < arr[j][1]:
+            rank += 1
+    ranks.append(rank)
+
+print(*ranks)
