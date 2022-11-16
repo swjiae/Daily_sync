2f4d3f39b6fe659f4902db7f91eb736cd5644332
swjiae <jiae1625@gmail.com>
2022-11-17 08:03:18 +0900

    11/17

---
+++ JavaProject/src/baekjoon/Bangkok.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Bangkok {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int year = in.nextInt();
+        in.close();
+
+        System.out.println(year-543);
+    }
+}
